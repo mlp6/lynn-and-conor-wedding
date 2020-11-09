fe8497cd836d96fe11c8bc0e461fbe963c9e4008
Mark Palmeri <mlp6@duke.edu>
2020-11-09 13:09:53 -0500

    get email script in shape

--- send_invitation_emails.py
+++ send_invitation_emails.py
@@ -1,5 +1,25 @@
 import pandas as pd
+import os
 
-names_emails = pd.read_csv("invite_list.csv")
+names_emails = pd.read_csv("invite_list.csv", skipinitialspace=True,
+                           dtype={'Name01':str,'Name02':str,'Email':str})
 
-#mail -r "lynn-and-conor-wedding@palmeri.io (Lynn & Conor)" -s "Lynn Palmeri and Conor Beardsley Wedding" EMAIL_ADDRESS < email_body.txt
+for n, invite in names_emails.iterrows():
+    if isinstance(invite["Name02"], str):
+        dear = f'Dear {invite["Name01"]} and {invite["Name02"]},'
+    else:
+        dear = f'Dear {invite["Name01"]},'
+
+    email = f"{dear}\n\nYou are cordially invited to attend the wedding ceremony and reception of Lynn Palmeri and Conor Beardsley.\n\nPlease visit the following website for information and RSVP instructions: http://lynn-and-conor-wedding.palmeri.io\n\nWe hope to see you in February!\nLynn and Conor\n\n"
+
+    email_file = f"email_body.txt"
+
+    with open(email_file, "w") as email_body:
+        email_body.write(email)
+
+    cmd = f'mail -r "lynn-and-conor-wedding@palmeri.io (Lynn & Conor)" -s "Lynn Palmeri and Conor Beardsley Wedding" {invite["Email"]} < {email_file}'
+
+    os.system(f"cat {email_file}")
+    print(cmd)
+
+    os.remove(email_file)
