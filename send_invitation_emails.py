2735c53ba9cdf14f7d9b2fa0519e6645c1f529f1
Mark Palmeri <mlp6@duke.edu>
2020-11-09 12:07:41 -0500

    add starting point for email list

---
+++ send_invitation_emails.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+names_emails = pd.read_csv("invite_list.csv")
+
+#mail -r "lynn-and-conor-wedding@palmeri.io (Lynn & Conor)" -s "Lynn Palmeri and Conor Beardsley Wedding" EMAIL_ADDRESS < email_body.txt
